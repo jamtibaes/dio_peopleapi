9f35337ce9ec60471f0c771b530250607dbebbc6
jamtibaes <jamtibaes@yahoo.com.br>
2022-01-28 23:12:19 -0300

    Developed person create operation

---
+++ src/main/java/com/digitalinnovationone/personapi/enums/PhoneType.java
@@ -0,0 +1,14 @@
+package com.digitalinnovationone.personapi.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum PhoneType {
+    NOME("Nome"),
+    MOBILE("Mobile"),
+    COMMERCIAL("Commercial");
+
+    private final String description;
+}
